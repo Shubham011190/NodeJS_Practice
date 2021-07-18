1979dc4bc499d0488f9862bba3b67d68756a346a
Shubham Singhal <shubham.011190@gmail.com>
2021-07-18 18:32:41 +0530

    Async

--- app.js
+++ app.js
@@ -1,31 +1,8 @@
-const { readFile, writeFile} = require('fs');
-const util = require('util');
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
+const http = require('http');
+const server = http.createServer();
 
+server.on('request', (req,res)=>{
+    console.log("Welcome");
+})
 
-const start = async () => {
-    try {
-        const first = await readFilePromise('./content/first.txt','utf8');
-        const second = await readFilePromise('./content/second.txt','utf8');
-        await writeFilePromise('./content/promisifyResult.txt',`${first} , ${second}`);
-        console.log(first,second);
-    } catch (error) {
-        console.log(error);
-    }
-}
-start()
-
-
-// getText('./content/first.txt').then((result) => console.log(result)).catch((err) => console.log(err))
-
-// const getText = (path) => {
-//     return new Promise((resolve, reject) => {
-//         readFile(path, 'utf8', (err, data) => {
-//             if (err)
-//                 reject(err)
-//             else
-//                 resolve(data)
-//         })
-//     })
-// }
+server.listen(5000);

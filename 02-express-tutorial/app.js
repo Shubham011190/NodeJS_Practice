e19c9ab8cbc3e7498723598384ed7f5b0c649d82
Shubham Singhal <shubham.011190@gmail.com>
2021-07-19 21:49:07 +0530

    http headers

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log("User started server");
+    console.log(req);
+
+    // console.log("User started server");
+    res.writeHead(200,{'content-type': 'text/html'})
+    res.write('<h1>Homepage</h1>')
+    res.end()
 });
 
 server.listen(5000);

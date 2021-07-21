f0da99d5c5eb8adde79ac2b7549076267cc15129
Shubham Singhal <shubham.011190@gmail.com>
2021-07-21 19:00:50 +0530

    express

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,64 +1,18 @@
-const http = require('http');
-const {readFileSync} = require('fs')
+const express = require('express');
+const app = express();
+const path = require('path');
 
-//get all files
-const homePage = readFileSync('./navbar-app/index.html')
-const homeStyles = readFileSync('./navbar-app/styles.css')
-const homeLogo = readFileSync('./navbar-app/logo.svg')
-const homeApp = readFileSync('./navbar-app/browser-app.js')
+//setting up static and middleware
+app.use(express.static('./public'))
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    //homepage
-    if (url === '/') {
-        // console.log("User started server");
-        res.writeHead(200, {
-            'content-type': 'text/html'
-        })
-        res.write(homePage)
-        res.end()
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./navbar-app/index.html'))
+})
 
-        //about
-    } else if (url === '/about') {
-        res.writeHead(200, {
-            'content-type': 'text/html'
-        })
-        res.write('<h1>About Page</h1>')
-        res.end()
-    }
-    //Styles
-    else if (url === '/styles.css') {
-        res.writeHead(200, {
-            'content-type': 'text/css'
-        })
-        res.write(homeStyles)
-        res.end()
-    }
-    //Logo
-    else if (url === '/logo.svg') {
-        res.writeHead(200, {
-            'content-type': 'image/svg+xml'
-        })
-        res.write(homeLogo)
-        res.end()
-    }
+app.all('*',(req,res)=>{
+    res.status(404).send("Resource not found")
+})
 
-    //Javascript
-    else if (url === '/browser-app.js') {
-        res.writeHead(200, {
-            'content-type': 'text/javascript'
-        })
-        res.write(homeApp )
-        res.end()
-    }
-    //Not Found
-    else{
-        res.writeHead(404, {
-            'content-type': 'text/html'
-        })
-        res.write('<h1>Page Not Found</h1>')
-        res.end()
-    }
-});
-
-server.listen(5000);
+app.listen(5000,()=>{
+    console.log("Server running at port 5000");
+})
